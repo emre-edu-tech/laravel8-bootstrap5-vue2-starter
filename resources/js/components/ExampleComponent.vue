7d409c01bf91ab1dbdf326f857d0ed9f0e09effc
emre-edu-tech <emreebrukayikcilar@gmail.com>
2022-01-26 10:00:50 +0300

    Initial Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ count }}
+        <button @click="addOne()">Increment 1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        addOne(){
+            return this.count++;
+        }
+    },
+    mounted() {
+        console.log('Example Component Loaded');
+    }
+}
+</script>
